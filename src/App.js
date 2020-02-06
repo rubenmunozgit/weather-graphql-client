0594bc7b9a0a63589857ab387b64ecc3b49f6f2c
rpalomino <rpalomino@homeaway.com>
2020-02-06 12:08:31 +0100

    added graphql clent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
 import './App.css';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const GET_WEATHER = gql`
+  {
+    weather(city: "Berlin, DE") {
+      current {
+        temperature,
+        feels_like
+      }
+    }
+  }
+`;
+
+const Weather = props => {
+  const { loading, error, data } = useQuery(GET_WEATHER);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (data) {
+    console.log(data)
+    const {weather} = data;
+    const {current: {temperature, feels_like}} = weather;
+    return (
+      <div className="currentWeather__Temp">
+        <div>{temperature}</div>
+        <div className="currentWeather__Feel">feels like: {feels_like}</div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <h3>React up un running...</h3>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h3>React up un running...</h3>
+        <Weather />
+      </div>
+    </ApolloProvider>
   );
 }
 
